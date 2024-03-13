1276b875261f37ef38c5f98ff98bcca59c4f82d0
Lunachar <vasiljev.michael@gmail.com>
2024-03-13 13:05:35 +0300

    add gameManager

---
+++ Assets/Scripts/RemoveAfterDelay.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveAfterDelay : MonoBehaviour
+{
+    public float delay = 1.0f;
+
+    private void Start()
+    {
+        StartCoroutine("Remove");
+    }
+
+    IEnumerator Remove()
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Resettable.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Resettable : MonoBehaviour
+{
+    public UnityEvent onReset;
+
+    public void Reset()
+    {
+        onReset.Invoke();
+    }
+}
